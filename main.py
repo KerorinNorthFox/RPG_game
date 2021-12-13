a669178b0db20a862bdcdabae3ebde600d6cd724
m20_KerorinNF <66837061+KerorinNorthFox@users.noreply.github.com>
2021-12-13 15:34:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from CharacterClass import Character
 
+charaA = Character("A", 100, 0, 50, 10, 40, 5, 10)
+charaB = Character("B", 200, 0, 50, 10, 40, 5, 10)
+
+charaA.magicalAttack(charaB)
+charaA.showStatus()
+charaB.showStatus()
+print("############################")
+charaB.magicalAttack(charaA)
+charaA.showStatus()
+charaB.showStatus()

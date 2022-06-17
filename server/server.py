a5a8a308aca62528110844635070a925588bb3dd
KerorinNF <kitakitsune.gingitsune.8220A@gmail.com>
2022-06-17 17:59:39 +0900

    add set_obj route, issue: send list by using json

--- server/server.py
+++ server/server.py
@@ -11,7 +11,6 @@ app = Flask(__name__)
 def obj_to_byte(self, obj:object) -> bytes:
     return bz2.compress(pickle.dumps(obj, pickle.HIGHEST_PROTOCOL), 3)
 
-
 # バイト列→オブジェクト
 def byte_to_obj(self, b:bytes) -> object:
     return pickle.loads(bz2.decompress(b))
@@ -21,6 +20,7 @@ def byte_to_obj(self, b:bytes) -> object:
 def save_obj():
     user_data: str | bytes = request.data.decode('utf-8')
     username, party_obj_list, world_obj = user_data
+
     print(f">>Save objects by {username}")
 
     conn: object = sqlite3.connect('users.db')
@@ -35,6 +35,7 @@ def save_obj():
     b: bytes = obj_to_byte(party_obj_list[3])
     cursor.execute("UPDATE Users SET sage_obj = ? WHERE username = ?", ((b, username)))
 
+    world_obj.save = True
     b: bytes = obj_to_byte(world_obj)
     cursor.execute("UPDATE Users SET world_obj = ? WHERE username = ?", ((b, username)))
 
@@ -42,6 +43,41 @@ def save_obj():
     conn.commit()
     conn.close()
     
-    world_obj.save = True
+    return Response(response=b, status=200)
 
+@app.route("/set_obj", methods=['POST'])
+def set_obj():
+    username = request.data.decode('utf-8')
+    
 
+    conn: object = sqlite3.connect('users.db')
+    cursor: object = conn.cursor()
+    Party: list[object] = []
+
+    cursor.execute("SELECT hero_obj FROM Users where username = ?", ((username,)))
+    chara: tuple[bytes] = cursor.fetchone()
+    Party.append(byte_to_obj(chara[0]))
+    cursor.execute("SELECT fencer_obj FROM Users where username = ?", ((username,)))
+    chara: tuple[bytes] = cursor.fetchone()
+    Party.append(byte_to_obj(chara[0]))
+    cursor.execute("SELECT wizard_obj FROM Users where username = ?", ((username,)))
+    chara: tuple[bytes] = cursor.fetchone()
+    Party.append(byte_to_obj(chara[0]))
+    cursor.execute("SELECT sage_obj FROM Users where username = ?", ((username,)))
+    chara: tuple[bytes] = cursor.fetchone()
+    Party.append(byte_to_obj(chara[0]))
+
+    cursor.execute("SELECT world_obj FROM Users where username = ?", ((username,)))
+    world: tuple[bytes] = cursor.fetchone()
+    World: object = byte_to_obj(world[0])
+
+    cursor.close()
+    conn.close()
+
+    list_1 = [Party, World]
+    list_1 = bz2.compress(list_1, 3)
+
+    return Party, World
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, threaded=True)
